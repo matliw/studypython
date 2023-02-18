22567e4a7176721942642b570438e7951bbed6e5
Mateusz Liwinski <Mateusz.Liwinski@dataart.com>
2023-02-18 19:29:00 +0100

    Added additional notes to args/kwargs, added descriptor

--- 3_args_kwargs/args_kwargs.py
+++ 3_args_kwargs/args_kwargs.py
@@ -15,8 +15,37 @@ def only_default_arguments(weapon_type, damage_type='PHYSICAL', damage=1):
         f'Your weapon is {weapon_type} type and causes {damage_type} damage of {damage} points!')
 
 
+def take_args(standard_arg, standard_arg_2, *args, **kwargs):
+    """1. *args take any number of positional arguments, **kwargs takes any number of keyword arguments
+       2. * unpacks all iterables / ** unpacks dictionary
+       3. * unpacks contents - so unpacked list is no longer a list
+       4. unpacking to a method that requires 3 arguments must match the number of required args
+       5. ordered as inputted"""
+
+    my_args = args  # returns a tuple
+    my_kwargs = kwargs  # keyword arguments return a dictionary
+    a = []
+    for arg_item in args:
+        a.append(arg_item)
+    k = [kwarg_item for kwarg_item in
+         kwargs.values()]  # needs .values() to return data values from dict
+    return standard_arg, standard_arg_2, a, k
+
+
+def merge_lists(list_a, list_b):
+    """Can also merge dictionaries"""
+    print('Merged lists: ' + str([*list_a, *list_b]))
+
+
 if __name__ == '__main__':
-    only_keyword_named_arguments(damage_type='fire', weapon_type='bow', damage=5)
+    only_keyword_named_arguments(damage_type='fire', weapon_type='bow',
+                                 damage=5)
     only_positional_arguments('toothpick', 'ice', 7)
     #  only_positional_arguments(7, 'toothpick', damage='ice')  #Fails because 3 POSITIONAL arguments are required
-    only_default_arguments('screwdriver')  # positional but if one of the arguments is missing it will be filled in by default values from function
+    only_default_arguments(
+        'screwdriver')  # positional but if one of the arguments is missing it will be filled in by default values from function
+    take_args(1, 2, 3, 4, a='A', b='B', c='D')
+    take_args(1, 2, a=3)  # no args were passed
+    list_1 = [12, 13, 14]
+    list_2 = [15, 16, 17]
+    merge_lists(list_1, list_2)
---
+++ 5_descriptors/descriptor.py
@@ -0,0 +1,55 @@
+"""implement your own descriptor that logs when attrs of a class is modified"""
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class AttributeLogger:
+
+    def __set_name__(self, owner,
+                     name):  # records field names. So, we can access more than one field.
+        self.public_name = name
+        self.private_name = '_' + name  # not sure why we need the protected private name
+
+    def __get__(self, obj,
+                objtype=None):  # self is attribute; obj is instance of a class; objtype is CLASS
+        value = getattr(obj, self.private_name)
+        logging.info(
+            'Accessing CLASS %r and attribute value %r. CLASS name is %r + self is %r',
+            self.public_name, value, objtype.__name__, self.__dict__)
+        return value  # what happens if I don't return it?
+
+    def __set__(self, obj, value):
+        if isinstance(value, str):  # check if string type, raise error if not. Can be used to create validators
+            self.public_name = value
+        else:
+            raise TypeError(
+                f'{value} is {type(value)} and it should be string value.')
+        logging.info('Update CLASS %r and attribute value %r',
+                     self.public_name, value)
+        setattr(obj, self.private_name, value)
+
+    def __delete__(self, obj):
+        logging.info('Deleted %r attribute',
+                     self.public_name)
+        del self.public_name
+
+
+class Developers:
+    dev_name = AttributeLogger()
+    programming_language = AttributeLogger()
+
+    def __init__(self, dev_name, programming_language):
+        self.dev_name = dev_name
+        self.programming_language = programming_language
+
+
+class AttrLogggerWithProperty:
+
+
+
+if __name__ == '__main__':
+    Mateusz = Developers('Mateusz', 'Python')
+    Mats_language = Mateusz.programming_language
+    del Mateusz.programming_language
+    Ivan = Developers(1, 2)
